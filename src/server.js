1a3c2d663d2860b1ae14e5b52778ab9209b93b14
eduardoabreu81 <eduardoabreu81@gmail.com>
2025-05-21 16:54:46 -0300

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -19,6 +19,9 @@ app.use(cors());
 app.use(express.json());
 app.use(i18n.init);
 
+const authRoutes = require('./routes/auth.routes');
+app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => {
   res.send(res.__('Bem-vindo ao backend do Project.AI!'));
 });
